7ccb691e34ce9c555667630005c60aaf3df65194
Squire-DeAnna <booklover1123@gmail.com>
2018-01-17 18:23:07 -0700

    deleted files
    deleted extra files

--- OhTheFreePlaces/common/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Oh the Free Places You'll Go!</title>
-    <link href="../css/style.css" type="text/css" rel="stylesheet">
-</head>
-
-
-<h1>Oh the Free Places You'll Go!</h1>
--- OhTheFreePlaces/library/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dsn = 'mysql:host=localhost;dbname=freeplac_schema';
-    $username = 'freeplac_admin';
-    $password = 'pa55word';
-
-
