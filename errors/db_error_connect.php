36d22bed62b98faf2e5a446910bef9f9583eeebd
Squire-DeAnna <booklover1123@gmail.com>
2018-01-19 12:40:56 -0700

    Update db_error_connect.php

--- errors/db_error_connect.php
+++ errors/db_error_connect.php
@@ -1,7 +1,11 @@
-<?php include '../view/header.php'; ?>
-<main>
-    <h1>Database Error</h1>
-    <p>An error occurred connecting to the database. Please try again</p>
-    <p>Error message: <?php echo $error_message; ?></p>
-</main>
-<?php include '../view/footer.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/common/header.php'; ?>
+
+        <h1 class='content-title'>Database Error</h1>
+        <p>An error occurred connecting to the database. Please try again</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-pinterest"></a>
+        
+<?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'; ?>
