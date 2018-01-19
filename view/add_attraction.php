7e092d783927dbd04d43695822dbfbef84ff2575
Squire-DeAnna <booklover1123@gmail.com>
2018-01-19 12:44:34 -0700

    Update add_attraction.php

--- view/add_attraction.php
+++ view/add_attraction.php
@@ -1,17 +1,12 @@
-<?php include '../common/header.php'; ?>
-<body>
-    <div class="menu">
-        <h2></h2>
-        <?php include '../common/nav.php'; ?>
-    </div>
-    <div class="leftSidebar">
-        <h2></h2>
-        <?php include ''; ?>
-    </div>
 
-    <div class="body"> 
-        <h2>Header here</h2>
-        Body text here
+    
+<?php include $_SERVER['DOCUMENT_ROOT'].'/common/header.php'; ?>
 
-    </div>
-    <?php include '../common/footer.php'; ?>
+        <h1 class='content-title'>Add Attraction</h1>
+        <p>Add a new attraction here:</p>
+
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-pinterest"></a>
+        
+<?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'; ?>
