b007bcda34901992358083243a37304abdf80d87
Squire-DeAnna <booklover1123@gmail.com>
2018-01-22 11:20:12 -0700

    Update city_list.php

--- view/city_list.php
+++ view/city_list.php
@@ -1,16 +1,7 @@
-<?php include '../common/header.php'; ?>
-<body>
-    <div class="menu">
-     <h2></h2>
-        <?php include '../common/nav.php'; ?>
-    </div>
-   <div class="leftSidebar">
-     <h2></h2>
+    <?php include 'common/header_2.php'; ?>
 
-    </div>
+        <h1 class='content-title'>List of Cities</h1>
         
-    <div class="body"> 
-        <h2>List of Cities</h2>
         <?php foreach ($cities as $city) : ?>
         <li>
             <a href="../attractions/index.php?action=view_attraction_list_by_city&amp;city_id=<?php echo $city->getID(); ?>">
@@ -18,6 +9,5 @@
                 </a>  
         </li>
         <?php endforeach; ?>
-        
-    </div>
-<?php include '../common/footer.php'; ?>
+
+<?php include 'common/footer.php'; ?>
