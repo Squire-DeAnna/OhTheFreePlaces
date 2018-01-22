a15b693852c684aa811e0d39135fdf638450d720
Squire-DeAnna <booklover1123@gmail.com>
2018-01-22 11:29:57 -0700

    Changing the view files to match the template

---
+++ view/user_mylist.php
@@ -0,0 +1,6 @@
+    <?php include 'common/header_2.php'; ?>
+
+        <h1 class='content-title'>My List of Attractions</h1>
+        <p>This is where the list of attractions they have created will sit. </p>
+        
+    <?php include 'common/footer.php'; ?>
